e107434a88ed0fa9d97b1fb8c63c4247b26c1570
ebenezerjoseph <84997555+ebenezerjoseph@users.noreply.github.com>
2021-09-14 21:25:11 +0530

    Update assignment_04.py

--- Section_02/assignment_04.py
+++ Section_02/assignment_04.py
@@ -9,6 +9,16 @@ my_list = [(1, 2), (3, 4), (['c', 'd', 'a', 'm'], [3, 9, 4, 12], 4), 'TV', 42]
 
 # Your Code Below:
 
+my_list = [(1, 2), (3, 4), (['c', 'd', 'a', 'm'], [3, 9, 4, 12], 4), 'TV', 42]
+
+my_list[2][0][-1]='x'
+
+my_list[3] = 'Television'
+
+print(my_list)
+
+
+
 
 
 
